2b1f68e1dde73e60e77728a674e2cb9eb4e07cca
TomChambers <ThomasChambers15243@outlook.com>
2024-09-12 10:48:28 +0100

    Changes

--- src/linker.ts
+++ src/linker.ts
@@ -30,8 +30,9 @@ export class AutoLinker{
                 const headers = extractHeadersFromFile(this.app.metadataCache, file);
         
                 // Add file names
-                this.addLinkToMap(file.basename, `[${file.basename}](${file.path})`);
-        
+                this.addLinkToMap(file.basename, `[${file.basename}](${replaceSpaces(file.path)})`);
+                
+
                 // Add tags
                 tags.forEach(tag => {
                     this.addLinkToMap(tag, `#${tag}`)
@@ -39,7 +40,8 @@ export class AutoLinker{
         
                 // Add headers
                 headers.forEach(header => {
-                        this.addLinkToMap(header.heading, `[${header.heading}](${file.path}#^${header.heading})`)
+                        let link = `[${header.heading}](${replaceSpaces(file.path)}#^${replaceSpaces(header.heading)})`
+                        this.addLinkToMap(header.heading, link)
                 });
             }
             
@@ -57,28 +59,41 @@ export class AutoLinker{
             const cursor = editor.getCursor();
             const line = editor.getLine(cursor.line);
             if (this.linkMap != undefined) {
-                const matches = this.lineHasKey(line, Array.from(this.linkMap.keys()));
-                console.log("Matches are: " , matches);
-                if (matches != null) {
-                    // Always take the longest match as its most likley this was the intended one
-                    const match = matches.reduce((longest, current) => 
-                        current.length > longest.length ? current : longest, "");
-                    console.log("Match is: " + match)
-                    // Get the bucket
-                    const links = this.linkMap.get(match);
-                    if (links !== undefined) {
-                        let newLine = "";
-                        if (links.length > 1) {
-                            const modal = new LinkSelectionModal(this.app, links);
-                            newLine = await modal.openAndGetSelection();
-                        } else {
-                            newLine = line.replace(match, links[0]);
+                let newLine = line;
+                let matches: RegExpMatchArray | null;
+                let count = 0;
+                do {
+                    matches = this.lineHasKey(newLine, Array.from(this.linkMap.keys()));
+                    console.log("Matches are: " , matches);
+                    if (matches != null) {
+                        // Always take the longest match as its most likley this was the intended one
+                        let match = matches.reduce((longest, current) => 
+                            current.length > longest.length ? current : longest, "");
+                        console.log("Match is: " + match)
+                        // Get the bucket
+                        let links = this.linkMap.get(match.trim());
+                        console.log(links)
+                        console.log(this.linkMap);
+                        if (links !== undefined) {
+                            let linkText = links[0]
+                            // If there are colling links, get the user to select one from a list
+                            if (links.length > 1) {
+                                const modal = new LinkSelectionModal(this.app, links);
+                                linkText = await modal.openAndGetSelection();
+                            }
+                            console.log("newline vefore", newLine)
+                            newLine = newLine.replace(match, linkText);                                     
+                            console.log("newline after", newLine)
                         }
-                        // Replace match
-                        editor.replaceRange(newLine+" ", 
-                            { line: cursor.line, ch: 0 },
-                            { line: cursor.line, ch: line.length });                                       
                     }
+                    count += 1;                    
+                }
+                while (matches && count <= line.length);
+                // Replace match
+                if (newLine != line) {
+                    editor.replaceRange(newLine, 
+                        { line: cursor.line, ch: 0 },
+                        { line: cursor.line, ch: line.length });  
                 }
             } else {
                 console.log("ERROR: Map is undefined");
@@ -91,16 +106,17 @@ export class AutoLinker{
         // Stops any ghost links being inserted        
         // so the user doesn't get spooked
         if (name == "") {return}
-    
+
         let bucket = this.linkMap.get(name);
         // If its undefined then its unique so inserted
         if (bucket === undefined) {
-            this.linkMap.set(name, [link]);
+            this.linkMap.set(name.trim(), [link]);
         // If theres a collision then its not unique so push to collections
         } else if (!bucket.contains(link)) {
             bucket.push(link)
         }
     }
+    
     private lineHasKey(line: string, keys: string[]): RegExpMatchArray | null {
         const keysP = keys.map(key => key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|");
 
@@ -159,4 +175,7 @@ function extractTagsFromFile(metadataCache: MetadataCache, file: TFile): string[
     
   }
 
+function replaceSpaces(str: string): string {
+    return str.replaceAll(" ", "%20")
+}
   
